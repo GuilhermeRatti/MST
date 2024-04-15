900f44cdcaaaa88eeb1298db1c097c46503adbc7
GuilhermeRatti <guirattimoraes@gmail.com>
2024-04-15 14:42:49 -0300

    comentarios aresta

--- Aresta.c
+++ Aresta.c
@@ -26,57 +26,51 @@ static Aresta _aresta_cria(int idx_p1, int idx_p2, MACRO_TAMANHO distancia)
     return aresta;
 }
 
+// O proposito de usar min heap eh que conseguimos acessar o menor valor com tempo constante e reorganizar a heap com tempo de logN
+// No final, usando heap ao inves de Qsort teremos um pequeno ganho de velocidade, pois apesar de o pior caso dos 2 ser ~NlogN
+// O heap sera chamado somente ~(P-K)logA, sendo P o numero de pontos, K o numero de clusters desejados e A o numero de arestas,
+// tal que A >> P e K; o heap pode demorar um pouco mais de acordo com a qtd de ligacoes circulares encontradas. 
+// O qsort por outro lado tera pior caso como ~AlogA, oq eh muito maior, pois eh necessario percorrer o vetor de arestas A inteiro
 void _min_heapify(int quantidade_arestas, Aresta *vetor_arestas, int idx)
 {
     int filho_esquerda = 2*idx+1, filho_direita = 2*idx + 2, menor_val = idx;
     
+    // Verifica se o filho da esquerda nao estourou o limite
     if(filho_esquerda < quantidade_arestas)
     {
-        
+        // Verifica se o filho da esquerda eh menor que o pai
         if(vetor_arestas[filho_esquerda].distancia < vetor_arestas[idx].distancia)
         {
             menor_val = filho_esquerda;
         }
     }
+    // Verifica se o filho da direita nao estourou o limite
     if(filho_direita < quantidade_arestas)
     {
+        // Verifica se o filho da direita eh menor que o filho da esquerda ou o pai (depende do resultado da operacao acima)
         if(vetor_arestas[filho_direita].distancia < vetor_arestas[menor_val].distancia)
         {
             menor_val = filho_direita;
         }
     }
     
+    // Ve se uma troca eh necessaria
     if(menor_val != idx)
     {
         Aresta aux = vetor_arestas[idx];
         vetor_arestas[idx] = vetor_arestas[menor_val];
         vetor_arestas[menor_val] = aux;
+        // Chamada recursiva da funcao
         _min_heapify(quantidade_arestas,vetor_arestas,menor_val);
     }
 }       
 
+// Transforma um vetor desordenado em um heap
 void _build_heap(int quantidade_arestas, Aresta *vetor_arestas)
 {
+    //Complexidade ~N
     for(int i = (quantidade_arestas/2-1); i>=0; i--)
         _min_heapify(quantidade_arestas, vetor_arestas, i);
-}
-
-void _define_clusters(pPonto *pontos, int limite_unioes, int quantidade_arestas, Aresta *arestas)
-{
-    int unioes_feitas = 0;
-    int vertices[2] = {-1,-1};
-
-    while (unioes_feitas < limite_unioes)
-    {
-        Aresta menor_aresta = arestas[0];
-        arestas[0] = arestas[quantidade_arestas-1];
-        _min_heapify(quantidade_arestas-1,arestas,0); //LogN
-
-        // Tenta unir os vertices evitando unioes circulares (retorno 0)
-        if (UF_union(pontos, menor_aresta.idx_ponto1, menor_aresta.idx_ponto2)) unioes_feitas++;
-
-        quantidade_arestas--;
-    }
 }
 
 // Registro das distancias no vetor
@@ -87,12 +81,13 @@ void arestas_preenche_vetor(Aresta *vetor_arestas, pPonto *vetor_pontos, int qua
         for(int j=i+1; j<quantidade_pontos; j++) // Calcula a distancia entre os pontos vetor[i] e vetor[j] para registrar
             vetor_arestas[k++] = _aresta_cria(i,j,ponto_calcula_distancia(vetor_pontos[i], vetor_pontos[j], dimensoes));
     
-    // Ordenador das distancias
+    // Ordenador das distancias (no caso garante a propriedade de heap em que vetor[0] eh o menor valor possivel)
     _build_heap(quantidade_arestas,vetor_arestas);
 
     // _define_clusters(vetor_pontos, limite_unioes, quantidade_arestas, vetor_arestas);
 }
 
+// Retorna a menor aresta e reordena o vetor para que vet[0] seja o proximo menor valor
 void aresta_retorna_vertices_menor_distancia(Aresta *vetor_arestas, int quantidade_arestas, int vertices[])
 {   
     //printf("qtd_arestas: %d\n",quantidade_arestas);
@@ -104,12 +99,8 @@ void aresta_retorna_vertices_menor_distancia(Aresta *vetor_arestas, int quantida
     _min_heapify(quantidade_arestas-1,vetor_arestas,0); //LogN
 }
 
+// Destroi o vetor de arestas
 void aresta_destroi_vetor(Aresta *vetor_aresta)
 {
     free(vetor_aresta);
-}
-
-double aresta_retorna_distancia(Aresta a)
-{
-    return a.distancia;
 }
